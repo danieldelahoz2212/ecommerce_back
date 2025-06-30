10fb7af8d11e0796d160b2c85f835cda9cd3a0e7
Daniel <dany19992212@gmail.com>
2025-06-29 21:17:04 -0500

    se modifico la semilla usuario

--- seeders/20250630014342-demo-users.js
+++ seeders/20250630014342-demo-users.js
@@ -4,9 +4,8 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('users', [
-      { id: 1, name: 'Daniel', lastName: 'De La Hoz', email: 'admin@email.com', password: '123456', rol: 1, status: 1 },
-      { id: 2, name: 'Ana', lastName: 'Gomez', email: 'ana@email.com', password: '123456', rol: 2, status: 1 },
-      { id: 3, name: 'Luis', lastName: 'Perez', email: 'luis@email.com', password: '123456', rol: 3, status: 1 }
+      { name: 'Ana', lastName: 'Gomez', email: 'ana@email.com', password: '123456', rol: 2, status: 1 },
+      { name: 'Luis', lastName: 'Perez', email: 'luis@email.com', password: '123456', rol: 3, status: 1 }
     ], {});
   },
 
