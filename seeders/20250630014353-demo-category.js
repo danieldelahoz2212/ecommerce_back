d07213223a4aa9cf2c14d950a319443175b12045
Daniel <dany19992212@gmail.com>
2025-07-15 21:40:15 -0500

    se mejoró la consistencia del código en los seeders de categorías y estados de pedidos, ajustando el formato y la indentación

--- seeders/20250630014353-demo-category.js
+++ seeders/20250630014353-demo-category.js
@@ -1,16 +1,20 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('category', [
-      { id: 1, name: 'Electrónica', status: 1 },
-      { id: 2, name: 'Ropa', status: 1 },
-      { id: 3, name: 'Libros', status: 1 }
-    ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "category",
+      [
+        { id: 1, name: "Electrónica", status: 1 },
+        { id: 2, name: "Ropa", status: 1 },
+        { id: 3, name: "Libros", status: 1 },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('category', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("category", null, {});
+  },
 };
--- seeders/20250716021545-demo-stateorder.js
+++ seeders/20250716021545-demo-stateorder.js
@@ -1,17 +1,21 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-await queryInterface.bulkInsert('state_order', [
-    { id: 1, name: 'Pendiente', state: 1 },
-    { id: 2, name: 'Enviado', state: 2 },
-    { id: 3, name: 'Entregado', state: 3 },
-    { id: 4, name: 'Cancelado', state: 4 }
-  ], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "state_order",
+      [
+        { id: 1, name: "Pendiente", state: 1 },
+        { id: 2, name: "Enviado", state: 1 },
+        { id: 3, name: "Entregado", state: 1 },
+        { id: 4, name: "Cancelado", state: 1 },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('state_order', null, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("state_order", null, {});
+  },
 };
